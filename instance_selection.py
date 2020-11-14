38ab0de1ca43b0bbb4d26c080d88cfd2a7164a83
Eugenio Herrera <eiherrera@uc.cl>
2020-11-13 21:47:54 -0300

    Rework indices function

--- instance_selection.py
+++ instance_selection.py
@@ -83,7 +83,7 @@ def get_keep_indices(embeddings,
 
     cutoff = np.percentile(scores, (100 - retention_ratio))
     keep_mask = torch.from_numpy(scores > cutoff).bool()
-    keep_indices.append(paths[keep_mask])
+    keep_indices.append(np.array(paths)[keep_mask])
     keep_indices = torch.cat(keep_indices, dim=0)
     return keep_indices
 
