d3a1e57acbf6b9c694a4a53f61d92d94d18b42d2
Eugenio Herrera <eiherrera@uc.cl>
2020-11-13 21:52:18 -0300

    Rework indices function

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ def selecter(target_folder):
                                                      std=[0.5, 0.5, 0.5])])
     dataset = CustomDataset(target_folder, transform=transform)
     instance_selected_dataset = select_instances(dataset, retention_ratio=50)
-    print(instance_selected_dataset)
+    for el in instance_selected_dataset:
+        print(el)
 
 def main():
     parser = argparse.ArgumentParser(
